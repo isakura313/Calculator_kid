40bcf247c7f64e029f2d7be0203b6d705ba3af55
Paul <isakura313@gmail.com>
2020-07-13 13:45:41 +0300

    asdasd

---
+++ code.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let x = 12; // let говорит что переменная потом меняется!
+// const e = 2.71 //число эйлера const менять нельзя!
+// var y = 12; // тоже можно менять. можно сказать устаревший
+
+// function hello(){
+//     alert("привет! это JavaScript! ");
+// }
+// hello = () => {
+//     alert("привет! это JavaScript! ");
+// }
+// hello();
+// const Hulk = {
+//     color: "green",
+//     size: "big",
+//     crush: ()=>{
+//         alert(" Hulk CRUSHH!!!")
+//     }
+// }
+// Hulk.crush()
+// alert(Hulk.color)
+
+// let Avengers = ['Hulk', 'Iron Man', 'Captaion Amerika']
+// Avengers.push('SpiderMan')
+// alert(Avengers)
